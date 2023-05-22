607f9d1f39a6302750c57f8ed326583b5b3fba96
estg5 <buyankov.ilya@gmail.com>
2023-05-22 20:59:58 +0300

    1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,7 +4,7 @@ const Button = ({ setOnClick, text }) => (
   <button onClick={setOnClick}>{text}</button>
 );
 
-const DisplayCount = ({ text, count }) => (
+const DisplayInfo = ({ text, count }) => (
   <p>
     {text} {count}
   </p>
@@ -17,17 +17,40 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [average, setAverage] = useState(0);
+
+  const handleGoodOnClick = () => {
+    const goodToSet = good + 1;
+    setAverage(average + 1);
+    setGood(goodToSet);
+  };
+
+  const handleNeutralOnClick = () => {
+    const neutralToSet = neutral + 1;
+    setNeutral(neutralToSet);
+  };
+
+  const handleBadOnClick = () => {
+    const badToSet = bad + 1;
+    setAverage(average - 1);
+    setBad(badToSet);
+  };
+
+  const getAll = () => good + bad + neutral;
 
   return (
     <div>
       <Heading heading="give feedback" />
-      <Button setOnClick={() => setGood(good + 1)} text="good" />
-      <Button setOnClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button setOnClick={() => setBad(bad + 1)} text="bad" />
+      <Button setOnClick={handleGoodOnClick} text="good" />
+      <Button setOnClick={handleNeutralOnClick} text="neutral" />
+      <Button setOnClick={handleBadOnClick} text="bad" />
       <Heading heading="statistics" />
-      <DisplayCount text="good" count={good} />
-      <DisplayCount text="neutral" count={neutral} />
-      <DisplayCount text="bad" count={bad} />
+      <DisplayInfo text="good" count={good} />
+      <DisplayInfo text="neutral" count={neutral} />
+      <DisplayInfo text="bad" count={bad} />
+      <DisplayInfo text="all" count={getAll()} />
+      <DisplayInfo text="average" count={average / getAll() || 0} />
+      <DisplayInfo text="positive" count={(good / getAll()) * 100 || 0} />
     </div>
   );
 };
