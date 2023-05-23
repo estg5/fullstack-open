1a4a51a662db3882e7e1ab547b3a51e14371044b
estg5 <buyankov.ilya@gmail.com>
2023-05-23 19:57:56 +0300

    2.17

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,12 +4,22 @@ import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 import personsService from "./services/persons";
 
+const Notification = ({ message, type }) => {
+  if (message === null) {
+    return <div></div>;
+  }
+
+  return <div className={type}>{message}</div>;
+};
+
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
   const [filteredPersons, setFilteredPersons] = useState(persons);
   const [filter, setFilter] = useState("");
+  const [notification, setNotification] = useState("");
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     personsService.getAll().then((resp) => {
@@ -54,18 +64,27 @@ const App = () => {
           )
         ) {
           const person = { ...persons[i], number: newPhone };
-          personsService.updatePerson(persons[i].id, person).then((resp) => {
-            const updatedPersons = persons.map((p) => {
-              if (resp.id === p.id) {
-                return { ...p, number: newPhone };
-              }
-              return p;
+          personsService
+            .updatePerson(persons[i].id, person)
+            .then((resp) => {
+              const updatedPersons = persons.map((p) => {
+                if (resp.id === p.id) {
+                  return { ...p, number: newPhone };
+                }
+                return p;
+              });
+              setPersons(updatedPersons);
+              setFilteredPersons(updatedPersons);
+              setNewName("");
+              setNewPhone("");
+            })
+            .catch(() => {
+              setMessage(`Cant update ${personToSave.name}`);
+              setNotification("error");
+              setTimeout(() => {
+                setMessage("");
+              }, 5000);
             });
-            setPersons(updatedPersons);
-            setFilteredPersons(updatedPersons);
-            setNewName("");
-            setNewPhone("");
-          });
         }
         return;
       }
@@ -81,21 +100,47 @@ const App = () => {
       setFilteredPersons(newPersons);
       setNewName("");
       setNewPhone("");
+
+      setMessage(`Added ${personToSave.name}`);
+      setNotification("success");
+      setTimeout(() => {
+        setMessage("");
+      }, 5000);
     });
   };
 
   const deleteOnClick = (id) => {
     if (window.confirm("wanna delete this sucker? ")) {
-      personsService.deletePerson(id).then((resp) => {
-        const newPersons = persons.filter((p) => p.id != id);
-        setPersons(newPersons);
-        setFilteredPersons(newPersons);
-      });
+      personsService
+        .deletePerson(id)
+        .then((resp) => {
+          const newPersons = persons.filter((p) => p.id != id);
+          setPersons(newPersons);
+          setFilteredPersons(newPersons);
+        })
+        .catch(() => {
+          const person = persons.filter((p) => p.id === id);
+          setMessage(
+            `Information of ${person[0].name} has already been removed from server`
+          );
+          setNotification("error");
+          const updatedPersons = persons.filter((p) => {
+            if (id !== p.id) {
+              return p;
+            }
+          });
+          setPersons(updatedPersons);
+          setFilteredPersons(updatedPersons);
+          setTimeout(() => {
+            setMessage("");
+          }, 5000);
+        });
     }
   };
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} type={notification} />
       <Filter value={filter} onChangeHandler={handleFilterChange} />
       <h2>add a new</h2>
       <PersonForm
