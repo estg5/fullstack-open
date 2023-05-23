a6110b1963e652c5cf87fc45b93033fb02ed9376
estg5 <buyankov.ilya@gmail.com>
2023-05-23 11:42:04 +0300

    2.7

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const addNewPerson = (e) => {
+    e.preventDefault();
+
+    for (let i = 0; i < persons.length; i++) {
+      if (persons[i].name === newName) {
+        alert(`${newName} is already added to phonebook`);
+        return;
+      }
+    }
+
+    setPersons(persons.concat({ name: newName }));
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewPerson}>
+        <div>
+          <label htmlFor="nameInput">Name:</label>
+          <input
+            placeholder="enter name..."
+            onChange={handleNameChange}
+            value={newName}
+            id="nameInput"
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((p) => (
+        <p key={p.name}>{p.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
